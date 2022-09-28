d247aa1621fc905c86d6460642bd54ad98bda0f9
Timm <sauegetier@googlemail.com>
2022-09-28 21:41:45 +0200

    change "date" into "datetime", add debug output

--- src/system.cpp
+++ src/system.cpp
@@ -1,5 +1,6 @@
 #include "system.h"
 #include <QProcess>
+#include <QDebug>
 
 System::System()
 {
@@ -18,15 +19,21 @@ void System::restart()
     process.startDetached("reboot");
 }
 
-bool System::setTime(QDate date)
+bool System::setTime(QDateTime date)
 {
     int result = -1;
     QDateTime time(date);
     struct timespec stime;
     time_t t = time.toTime_t();
     stime.tv_sec = t;
+
+    qDebug() << "Set Time " << date << " -- "  << t;
 #ifdef __linux__
     result = clock_settime(CLOCK_REALTIME, &stime); //return zero on success
+    if(result != 0)
+    {
+        qDebug() << "Setting time failed with error code: " << strerror(errno) << " (" << errno << ")";
+    }
 #elif __APPLE__
     #pragma message ( "setting the time is not implemented for MacOS" )
 #elif _WIN32
